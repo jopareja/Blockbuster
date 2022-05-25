902a23d28067c39d47d5ca7e3b47d975a3c5b9a6
josei <jose.ignacio97@gmail.com>
2022-05-24 21:20:11 -0300

    Deleted BindingAdapter that was not needed

--- app/src/main/java/com/example/blockbuster/BindingAdapters.kt
+++ app/src/main/java/com/example/blockbuster/BindingAdapters.kt
@@ -1,10 +1,8 @@
 package com.example.blockbuster
 
-import android.widget.ImageView
-import androidx.core.net.toUri
+
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
-import coil.load
 import com.example.blockbuster.network.Movie
 import com.example.blockbuster.ui.main.MovieGridAdapter
 
@@ -13,16 +11,4 @@ import com.example.blockbuster.ui.main.MovieGridAdapter
 fun bindRecyclerView(recyclerView: RecyclerView, data: List<Movie>?) {
     val adapter = recyclerView.adapter as MovieGridAdapter
     adapter.submitList(data)
-}
-
-// Adapter to Load the Image using COIL library
-@BindingAdapter("imageUrl")
-fun bindImage(imgView: ImageView, imgUrl: String?) {
-    imgUrl?.let {
-        val imgUri = imgUrl.toUri().buildUpon().scheme("https://image.tmdb.org/t/p/w185").build()
-        imgView.load(imgUri) {
-            placeholder(R.drawable.loading_animation)
-            error(R.drawable.ic_broken_image)
-        }
-    }
 }
