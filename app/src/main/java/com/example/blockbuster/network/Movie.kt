f7022e11a098012af24518ae285e8e5c77a8e18b
josei <jose.ignacio97@gmail.com>
2022-05-26 23:39:00 -0300

    Started Jetpack Navigation Component

--- app/src/main/java/com/example/blockbuster/network/Movie.kt
+++ app/src/main/java/com/example/blockbuster/network/Movie.kt
@@ -8,5 +8,8 @@ data class Movie(
     val id: Long,
     val title: String,
     val overview: String,
+    @Json(name = "release_date") val releaseDate: String,
     @Json(name = "poster_path") val imgSrcUrl: String,
+    @Json(name = "backdrop_path") val backdrop: String,
+    val popularity: Double
 )
