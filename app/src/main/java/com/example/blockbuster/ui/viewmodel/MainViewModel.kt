35bc2efc740c963890e20ed41670a35926c50134
josei <jose.ignacio97@gmail.com>
2022-05-29 16:29:44 -0300

    Cleaned Code

--- app/src/main/java/com/example/blockbuster/ui/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/blockbuster/ui/viewmodel/MainViewModel.kt
@@ -4,7 +4,6 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.blockbuster.data.Api
 import com.example.blockbuster.data.Movie
 import com.example.blockbuster.domain.GetPopularMoviesUseCase
 import kotlinx.coroutines.launch
