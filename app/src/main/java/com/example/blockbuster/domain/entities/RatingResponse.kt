b4b40d89d814deb2f06b86bf7196bae438eaa08d
Jose Ignacio Pareja <jose.ignacio97@gmail.com>
2022-06-21 23:03:28 -0300

    Changed RatingResponse

--- app/src/main/java/com/example/blockbuster/domain/entities/RatingResponse.kt
+++ app/src/main/java/com/example/blockbuster/domain/entities/RatingResponse.kt
@@ -3,5 +3,6 @@ package com.example.blockbuster.domain.entities
 import com.squareup.moshi.Json
 
 data class RatingResponse(
-    @Json(name = "value") val movieRating: Number
+    @Json(name = "status_code") val statusCode: Int,
+    @Json(name = "status_message") val statusMessage: String
 )
